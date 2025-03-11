30a120eb2a5b9a3b2672a671ad623e19f03f7c50
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2025-03-11 16:11:50 +0530

    module export correctly

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule} from '@nestjs/config';
 import { JwtStrategy } from "./strategies/jwt.strategies"
@@ -14,11 +13,13 @@ import refreshJwtConfig from './config/refresh-jwt.config';
 import { LocalStrategy } from './strategies/local.strategy';
 import { RefreshJwtStrategy } from './strategies/refresh.strategy';
 import googleOauthConfig from './config/google-oauth.config';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 
 
 @Module({
   imports:[
+   PrismaModule,
    JwtModule.registerAsync(jwtConfig.asProvider()),
    ConfigModule.forFeature(jwtConfig),
    ConfigModule.forFeature(refreshJwtConfig),
@@ -33,6 +34,6 @@ import googleOauthConfig from './config/google-oauth.config';
    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService,PrismaService,JwtStrategy,SetCookieInterceptor,JwtAuthGuard,GoogleStrategy,LocalStrategy,RefreshJwtStrategy]
+  providers: [AuthService,JwtStrategy,SetCookieInterceptor,JwtAuthGuard,GoogleStrategy,LocalStrategy,RefreshJwtStrategy]
 })
 export class AuthModule {}
