dcaa3efa2ff610beaa9152aa85170103477d8ec8
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2025-04-01 23:04:19 +0530

    all completed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module} from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
