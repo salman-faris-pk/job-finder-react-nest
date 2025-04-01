1f4d8383f7b9e1fa6da9d888ce31b12e9842d061
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2025-04-01 19:14:50 +0530

    just error

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,13 +5,12 @@ import { UsersModule } from './users/users.module';
 import { JobsModule } from './jobs/jobs.module';
 import { CompaniesModule } from './companies/companies.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
-import { CronService } from "./cron.service"
 
 @Module({
   imports: [
     PrismaModule, AuthModule, UsersModule, JobsModule, CompaniesModule, CloudinaryModule],
   controllers: [],
-  providers: [CronService],
+  providers: [],
 })
 
 export class AppModule {}
